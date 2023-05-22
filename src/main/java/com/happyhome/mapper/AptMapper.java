0e2dc63b6a33169f900f62dda0f7059bbcd66921
이동현 <dpwk8@naver.com>
2023-05-22 17:39:03 +0900

    FIX: MySQL Full_GROUP_BY Err 해결

--- src/main/java/com/happyhome/mapper/AptMapper.java
+++ src/main/java/com/happyhome/mapper/AptMapper.java
@@ -18,12 +18,22 @@ public interface AptMapper {
     @ResultType(AptDealAvgDto.class)
     List<AptDealAvgDto> showDealByRegion(String address);
 
-    @Select("select hi.aptcode, hi.aptName, dc.address, ss.name, round(avg(dealAmount/area),2) price " +
-            "from houseinfo hi " +
-            "left join sigungudongcode dc on hi.dongcode = dc.dongcode " +
-            "left join housedeal hd on hi.aptcode = hd.aptcode " +
-            "left join screeningstation ss on dc.sidogugun = ss.sidogugun " +
-            "where hi.aptcode = #{aptcode} group by hi.aptcode")
+//    @Select("select hi.aptcode, hi.aptName, dc.address, ss.name, round(avg(dealAmount/area),2) price " +
+//            "from houseinfo hi " +
+//            "left join sigungudongcode dc on hi.dongcode = dc.dongcode " +
+//            "left join housedeal hd on hi.aptcode = hd.aptcode " +
+//            "left join screeningstation ss on dc.sidogugun = ss.sidogugun " +
+//            "where hi.aptcode = #{aptcode} group by hi.aptcode, ss.name")
+
+    @Select("SELECT hi.aptcode, hi.aptName, dc.address, ss.name, ROUND(avg(dealAmount/area), 2) AS price "
+            + "FROM houseinfo hi "
+            + "LEFT JOIN sigungudongcode dc ON hi.dongcode = dc.dongcode "
+            + "LEFT JOIN housedeal hd ON hi.aptcode = hd.aptcode "
+            + "LEFT JOIN ( "
+            + "SELECT sidogugun, MIN(name) AS name FROM screeningstation GROUP BY sidogugun ) ss "
+            + "ON dc.sidogugun = ss.sidogugun "
+            + "WHERE hi.aptcode = #{aptcode} "
+            + "GROUP BY hi.aptcode, hi.aptName, dc.address, ss.name")
     AptDealAvgDetailDto showDetail(String aptCode);
 
     @Select("select dealAmount, dealYear, dealMonth, area, cancelDealType, aptCode "
