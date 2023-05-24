feddd56f50f3e550bbb2fa2382186c48d1e7a82d
eastsage <dpwk8@naver.com>
2023-05-25 01:03:52 +0900

    FIX: Cors Err 해결

--- src/main/java/com/happyhome/configuration/CorsConfig.java
+++ src/main/java/com/happyhome/configuration/CorsConfig.java
@@ -1,8 +1,11 @@
 package com.happyhome.configuration;
 
+import java.util.Arrays;
+import java.util.List;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -14,12 +17,16 @@ public class CorsConfig {
       UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
       CorsConfiguration config = new CorsConfiguration();
       config.setAllowCredentials(true);
-      config.addAllowedOrigin("*");
+//      config.addAllowedOrigin("*");
+//      config.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+      config.setAllowedOriginPatterns(Arrays.asList("http://localhost:8080"));
       config.addAllowedHeader("*");
       config.addAllowedMethod("*");
+      source.registerCorsConfiguration("/**", config);
 
-      source.registerCorsConfiguration("/vue/**", config);
       return new CorsFilter(source);
    }
+
+
 
 }
